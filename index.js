d369fce93bab4b59ca1b9386ddfa422b6a94638e
splashun <dcm93113@gmail.com>
2026-08-15 04:56:40 -0500

    again

--- index.js
+++ index.js
@@ -27,7 +27,7 @@ function createApp(database = pool) {
     );
 
     app.get('/', (req, res) => {
-        res.status(200).json({ message: 'the db will have entries at some point lol' }, { meantimeEntertainment: 'https://www.coolmathgames.com/0-papas-freezeria' });
+        res.status(200).json({ message: 'the db will have entries at some point lol', meantimeEntertainment: 'https://www.coolmathgames.com/0-papas-freezeria' });
     });
 
     app.get('/health', async (req, res) => {
