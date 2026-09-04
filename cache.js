54225724950f9d27bf209091b17d4e8308abfe8b
splashun <dcm93113@gmail.com>
2026-09-04 12:25:09 -0500

    ttl from 2hr to 24hr

--- cache.js
+++ cache.js
@@ -1,10 +1,10 @@
 const { LRUCache } = require('lru-cache');
 
-const TWO_HOURS = 2 * 60 * 60 * 1000;
+const TWENTY_FOUR_HOURS = 24 * 60 * 60 * 1000;
 
 const cache = new LRUCache({
   max: 10_000,
-  ttl: TWO_HOURS,
+  ttl: TWENTY_FOUR_HOURS,
 });
 
 module.exports = cache;
